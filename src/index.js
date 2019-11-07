47840c7b27ef608f80ea5603d0176870ace36975
sunimooni <jspark7373@naver.com>
2019-11-07 17:35:16 +0900

    setting env

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { CookiesProvider } from "react-cookie";
+import promise from "redux-promise";
+
+import reducers from "./reducers";
+import App from "./components";
+import "./style.scss";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <CookiesProvider>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+  </CookiesProvider>,
+  document.querySelector(".root")
+);
